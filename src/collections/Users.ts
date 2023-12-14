1414de1bb05e7fd0e3639d56f2a269832b2dc920
vovabatsyk <vovabatsyk@gmail.com>
2023-12-14 15:22:51 +0200

    create collections

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -15,6 +15,26 @@ export const Users: CollectionConfig = {
   },
   fields: [
     {
+      name: "products",
+      label: "Products",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "products",
+      hasMany: true,
+    },
+    {
+      name: "product_files",
+      label: "Product files",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "product_files",
+      hasMany: true,
+    },
+    {
       name: "role",
       defaultValue: "user",
       required: true,
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,10 +1,14 @@
 import { buildConfig } from "payload/config";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
-import { slateEditor } from "@payloadcms/richtext-slate";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
 import { Users } from "./collections/Users";
 import dotenv from "dotenv";
+import { Media } from "./collections/Media";
+import { ProductFiles } from "./collections/ProductFiles";
+import { Products } from "./collections/Products";
+import { Orders } from "./collections/Orders";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -12,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: "/sell",
   },
@@ -20,12 +24,14 @@ export default buildConfig({
     user: "users",
     bundler: webpackBundler(),
     meta: {
-      titleSuffix: " - DigitalHippo",
+      titleSuffix: "- DigitalHippo",
       favicon: "/favicon.ico",
       ogImage: "/thumbnail.jpg",
     },
   },
-  rateLimit: { max: 2000 },
+  rateLimit: {
+    max: 2000,
+  },
   editor: slateEditor({}),
   db: mongooseAdapter({
     url: process.env.MONGODB_URL!,
